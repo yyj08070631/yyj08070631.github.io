dc00d577d63e9a60cc878ca5c44fc47feae5186d
杨宇健 <yangyujian@yangyujiandeMacBook-Pro.local>
2021-10-31 16:50:46 +0800

    Publishing to master (deploy.sh)

--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,16 @@ set -e
 printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # 构建静态内容
-hugo --minify # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo --minify --destination "./docs" # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-# 切换到 Public 文件夹
-cd public
+# 切换到 docs 文件夹
+cd docs
 
 # 添加更改到 git
-git add .
+git add --all
 
 # 提交更改
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-msg="$*"
-fi
-git commit -m "$msg"
+git commit -m "Publishing to master (deploy.sh)"
 
 # 推送到远程仓库
 git push origin master
