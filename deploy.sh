135090534df3b0d73876fcfc9b07c20b1d3283ad
yangyujian <yangyujian@corp.netease.com>
2021-11-11 10:48:27 +0800

    rebuilding site Thu Nov 11 10:48:27     2021

--- deploy.sh
+++ deploy.sh
@@ -8,11 +8,14 @@ printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 # 构建静态内容
 hugo --minify # if using a theme, replace with `hugo -t <YOURTHEME>`
 
+# 远程目录设为 ssh 服务的 22 端口，防止本地修改了 ssh 默认端口
+git remote set-url origin ssh://git@github.com:22/yyj08070631/yyj08070631.github.io.git
+
 # 切换到 Public 文件夹
 cd public
 
 # 添加更改到 git
-git add .
+git add --all
 
 # 提交更改
 msg="rebuilding site $(date)"
