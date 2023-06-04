9e9b634278cf45d447c4824717f211407da858c2
yyj08070631 <934780820@qq.com>
2023-06-04 15:58:14 +0800

    Publishing to main (deploy.sh)

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# 任一步骤执行失败都会终止整个部署过程
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# 构建静态内容
+npm run build
+
+# 远程目录设为 ssh 服务的 22 端口，防止本地修改了 ssh 默认端口
+git remote set-url origin ssh://git@github.com:22/yyj08070631/yyj08070631.github.io.git
+
+# 添加更改到 git
+git add --all
+
+# 提交更改
+git commit -m "Publishing to main (deploy.sh)"
+
+# 推送到远程仓库
+git push --force origin main
